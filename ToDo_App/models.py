4a0873af4cadebdad350ea13e8ca520beda6832b
Arthur Henrique <Arthur.profissional20@gmail.com>
2023-07-19 18:46:47 -0300

    create model

--- ToDo_App/models.py
+++ ToDo_App/models.py
@@ -4,8 +4,17 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Task(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True) # null=True, blank=True allows for no user to be assigned to a task
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, blank=True) # null=True, blank=True allows for no user to be assigned to a task
     title = models.CharField(max_length=200) # max_length is required
-    description = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(null=True, blank=True)
     completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def  __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['completed']
+
+        
