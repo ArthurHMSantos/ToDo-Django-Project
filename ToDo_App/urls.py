7d654d71e0567460895603ba1f777035b07113aa
Arthur Henrique <Arthur.profissional20@gmail.com>
2023-07-23 13:04:35 -0300

    add update page

--- ToDo_App/urls.py
+++ ToDo_App/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import Home, TaskDetail
+from .views import Home, TaskCreate, TaskDetail, TaskUpdate
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
     path('task_detail/<int:pk>/', TaskDetail.as_view(), name='task_detail'),
+    path('task_form/', TaskCreate.as_view(), name='task_form'),
+    path('task_update/<int:pk>/', TaskUpdate.as_view(), name='task_update')
 ]
--- ToDo_App/views.py
+++ ToDo_App/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Task
 
 # Create your views here.
@@ -13,3 +13,16 @@ class TaskDetail(DetailView):
     model = Task
     template_name = 'ToDo_App/task_detail.html'
     context_object_name = 'task'
+
+class TaskCreate(CreateView):
+    model = Task
+    template_name = 'ToDo_App/task_form.html'
+    fields = '__all__'
+    success_url = reverse_lazy('home')
+
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    template_name = 'ToDo_App/task_form.html'
+    success_url = reverse_lazy('home')
