3c4235d43219c2eed2f6e76f531afd9b1a4dd66d
Arthur Henrique <Arthur.profissional20@gmail.com>
2023-07-24 10:48:46 -0300

    add user registration

--- ToDo_App/urls.py
+++ ToDo_App/urls.py
@@ -5,6 +5,7 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('login/', views.Login.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', views.Register.as_view(), name='register'),   
 
     path('', views.Home.as_view(), name='home'),
     path('task_detail/<int:pk>/', views.TaskDetail.as_view(), name='task_detail'),
--- ToDo_App/views.py
+++ ToDo_App/views.py
@@ -1,15 +1,36 @@
 from typing import Any, Dict
 from django.forms.models import BaseModelForm
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, FormView
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 from .models import Task
 
 
 # Create your views here.
+
+class Register(FormView):
+    template_name = 'ToDo_App/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(Register, self).form_valid(form)
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+        return super(Register, self).get(*args, **kwargs)
+
+
 
 class Login(LoginView):
     template_name = 'ToDo_App/login.html'
