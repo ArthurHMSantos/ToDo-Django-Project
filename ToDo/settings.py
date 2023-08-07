44df556162303a63011ae53d5eb0872b7f51ad18
Arthur Henrique <Arthur.profissional20@gmail.com>
2023-08-06 23:12:17 -0300

    vercel  try 3

--- ToDo/settings.py
+++ ToDo/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'ToDo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgreesql_psycopg2',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
